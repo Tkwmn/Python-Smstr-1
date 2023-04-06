1c0b58a0c2374f9cd5390e3a57c2da588681f53f
Alier Fres <Tkwmn@users.noreply.github.com>
2023-04-05 21:51:43 -0400

    first commit

---
+++ Hola.py
@@ -0,0 +1,21 @@
+print("hola soy Nikulias")
+
+nombre = "Nikulias"
+name = "Mati perkin"
+
+
+print(nombre)
+print("hola soy",nombre)
+
+
+edad = 18
+
+print("mi edad es de",edad)
+
+print("hola mi nombre es" " " + nombre + " "  "y tengo" + str(edad) +  "años de weon")
+
+
+nombre = "Yessi"
+print ("Hola mi nuevo nombre es", nombre)
+nombre = input("¿Cual es tu nombre?\n")
+print("tu nombre es:",nombre)
