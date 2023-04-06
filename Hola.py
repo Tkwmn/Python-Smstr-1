af6691bd493d50bc0e2aa4426a781d80577e54e0
Tkwmn <129216805+Tkwmn@users.noreply.github.com>
2023-04-05 22:41:51 -0400

    Add files via upload

---
+++ Hola.py
@@ -0,0 +1,21 @@
+print("hola soy Nicolas")
+
+nombre = "Nikulias"
+name = "Matias"
+
+
+print(nombre)
+print("hola soy",nombre)
+
+
+edad = 18
+
+print("mi edad es de",edad)
+
+print("hola mi nombre es" " " + nombre + " "  "y tengo" + str(edad) +  "años de edad")
+
+
+nombre = "Yessi"
+print ("Hola mi nuevo nombre es", nombre)
+nombre = input("¿Cual es tu nombre?\n")
+print("tu nombre es:",nombre)
