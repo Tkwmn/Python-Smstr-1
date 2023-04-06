74a064b48a826fad933590d12dd769d427c8c48b
Alier Fres <Tkwmn@users.noreply.github.com>
2023-04-06 16:20:57 -0400

    Clase 6 abril 2023

---
+++ tiposdedatos.py
@@ -0,0 +1,33 @@
+# Datos de un tipo numerico
+
+estatura = 1.71
+peso = 70
+complejo = 1+4j
+print("impresion del numero complejo",complejo)
+
+# Operaciones artimetica basica
+
+imc = peso/estatura**2
+print("Mi IMC es de:",imc)
+
+# Datos de tipo cadena de caracteres
+
+asignatura = 'programación'
+carrera = 'Ingenieria Civil en Informatica'
+print('La asignatura de',asignatura,'corresponde a la carrera de',carrera)
+
+# Valores booleanos
+
+ampolleta = False
+interruptor= True
+
+
+# Con type sabemos el tipo de datos  que estamos tratamdo
+
+print(type(ampolleta))
+
+# Datos tipo array (objetos de tipo colección)
+estudiantes = ['Matias', 'Marco', 'Cristobal', 'Sebastian']
+num = [1,2,3,4,5,6]
+print = (estudiantes)
+print = (num)
