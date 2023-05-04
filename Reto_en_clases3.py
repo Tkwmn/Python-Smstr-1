3b6dec084f78769c920fd215297cd316467e32fe
Alier Fres <Tkwmn@users.noreply.github.com>
2023-05-04 14:30:24 -0400

    3-5_mayo

---
+++ Reto_en_clases3.py
@@ -0,0 +1,11 @@
+ciudades = ["Santiago", "Temucho", "Osorno", "Punta Arenas"]
+indic_c_aire = [134, 99, 245, 50]
+
+indc_max = indic_c_aire. max[indic_c_aire]
+indc_min = min[indic_c_aire]
+
+ciud_min = ciudades(indic_c_aire.index(indc_min))
+ciud_max = ciudades(indic_c_aire.index(indc_max))
+
+print( "la ciudad ciudad con el indice mas bajo es",ciud_min)
+
