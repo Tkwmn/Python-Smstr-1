d80b9d72ec51f8b7856dedfaeec7c04090c2f594
Alier Fres <Tkwmn@users.noreply.github.com>
2023-05-11 13:29:49 -0400

    11-05

--- Programacion/Reto_en_clases4.py
+++ Programacion/Reto_en_clases4.py
@@ -3,42 +3,82 @@ print("Ingrese datos de 3 pacientes","\n")
 #----------------Paciente 1---------------#
 
 print("PACIETE 1°","\n")
-p1 = input("Ingrese el nombre del  paciente: ")
-peso_p1 = float(input("Indique el peso del paciente: "))
+p1 = input("Ingrese el nombre: ")
+peso_p1 = float(input("Indique el peso (Kg): "))
+
+while peso_p1 < 1:
+    print("Valor invalido")
+    peso_p1 = float(input("Indique el peso (Kg): "))
 
 
-est_p1 = float(input("Especifique estatura del paciente: "))
-ed_p1 = int(input("¿Cuál es la edad del paciente?: "))
+est_p1 = float(input("Especifique estatura: "))
+
+while est_p1 < 1:
+    print("Valor invalido")
+    est_p1 = float(input("Indique la estatura: "))
+else:
+    print("Valor Valido")
+
+ed_p1 = int(input("¿Cuál es la edad+?: "))
 
 while ed_p1 < 1 :
     print("Valor invalido")
-    ed_p1 = int(input("¿Cuál es la edad del paciente?: "))
+    ed_p1 = int(input("¿Cuál es la edad?: "))
 else:
     print("Valor valido","\n")
 
 #-----------------Paciente 2----------------#
 
 print("PACIETE 2°","\n")
-p2 = input("Ingrese el nombre del paciente: ")
-peso_p2 = float(input("Indique el peso del paciente: "))
-est_p2 = float(input("Especifique estatura del paciente: "))
-ed_p2 = int(input("¿Cuál es la edad de paciente?: "))
+p2 = input("Ingrese el nombre: ")
+
+peso_p2 = float(input("Indique el peso (Kg): "))
+
+while peso_p2 < 1:
+    print("Valor invalido")
+    peso_p2 = float(input("Indique el peso (Kg): "))
+else:
+    print("Valor Valido")
+
+est_p2 = float(input("Especifique estatura: "))
+
+while est_p2 < 1:
+    print("Valor invalido")
+    est_p2 = float(input("Indique estatura: "))
+else:
+    print("Valor Valido")
+
+ed_p2 = int(input("¿Cuál es la edad?: "))
 
 while ed_p2 < 1 :
-   ed_p1 = int(input("¿Cuál es la edad del paciente?: "))
+   ed_p1 = int(input("¿Cuál es la edad: "))
 else:
     print("Valor valido","\n")
 
 #-----------------Paciente 3----------------#
 
 print("PACIETE 3°","\n")
-p3 = input("Ingrese el nombre paciente: ")
-peso_p3 = float(input("Indique el peso paciente: "))
-est_p3 = float(input("Especifique estatura paciente: "))
-ed_p3 = int(input("¿Cuál es la edad del paciente?: "))
+p3 = input("Ingrese el nombre: ")
+peso_p3 = float(input("Indique el peso (Kg): "))
+
+while peso_p3 < 1:
+    print("Valor invalido")
+    peso_p3 = float(input("Indique el peso (Kg): "))
+else:
+    print("Valor Valido")
+
+est_p3 = float(input("Especifique estatura: "))
+
+while est_p3 < 1:
+    print("Valor invalido")
+    est_p3 = float(input("Indique estatura: "))
+else:
+    print("Valor Valido")
+
+ed_p3 = int(input("¿Cuál es la edad?: "))
 
 while ed_p3 < 1 :
-    ed_p1 = int(input("¿Cuál es la edad del paciente?: "))
+    ed_p1 = int(input("¿Cuál es la edad?: "))
 else:
     print("Valor valido","\n")
 
@@ -51,6 +91,7 @@ paciente3 = (p3, peso_p3, est_p3, ed_p3)
 #----------------IMPRESIÓN DE TUPLAS---------#
 
 print("             TUPLAS           ","\n")
+
 print(paciente1,"\n")
 
 print(paciente2,"\n")
