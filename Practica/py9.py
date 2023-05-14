e55a264645c478b038e04a552508be68df27b9d7
Alier Fres <Tkwmn@users.noreply.github.com>
2023-05-13 20:45:29 -0400

    13-5

---
+++ Practica/py9.py
@@ -0,0 +1,19 @@
+mes = input("ingrese un mes: ")
+
+while mes != "enero" and "febrero" and "marzo" and "abril" and "mayo" and "junio" and "julio" and "agosto" and "septiembre" and "otubre" and "noviembre" and "diciembre":
+    print("valor no coincide")
+    mes = input("ingrese un mes: ")
+
+if mes == "diciembre" or mes == "enero" or mes == "febrero": 
+    print("el mes de ",mes," esta en verano")
+else:
+    if mes == "marzo" or mes == "abril" or mes == "mayo":
+       print("el mes de ",mes," esta en otoño") 
+    else:
+        if mes == "junio" or mes == "julio" or mes == "agosto":
+            print("el mes de ",mes," esta en invierno") 
+        else:
+            if mes == "septiembre" or mes == "otubre" or mes == "novimebre":
+                print("el mes de ",mes," esta en primavera")
+
+
