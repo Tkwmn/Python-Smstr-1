b8df4c84705fca12be2bc60ca4509bd2b93ec903
Alier Fres <Tkwmn@users.noreply.github.com>
2023-05-17 15:51:35 -0400

    17-05

--- Guías/py9.py
+++ Guías/py9.py
@@ -1,27 +1,33 @@
 lista1 = [4,3,8,12,6,10,14,3,6]
-
 print("la lista esta compuesta de: ",lista1)
 
-del lista1[-1]
-
+lista1.pop(-1)
 print("la lista se modificó, ahora se compone de: ",lista1)
 
 lista1.insert(0,2)
+print("la lista se modificó, se ahora se compone de: ",lista1)
 
-print("la lista se modificó, ahora se compone de: ",lista1)
+lista1 = list(set(lista1))
+print("la lista se modificó, se eliminaron numeros repetidos, ahora se compone de: ",lista1)
 
-for i in lista1:
-    if lista1.count(i) > 1:
-        lista1.remove(i)
+#(--------------------------------------------)#
 
-print("la lista se modificó, ahora se compone de: ",lista1)
+lista1 = [4,3,8,12,6,10,14,3,6]
 
-media = round(sum(lista1)/len(lista1), 1)
+cantidad = len(lista1)
+suma = sum(lista1)
+media = round((suma/cantidad), 1)
 
-lista_ordenada = sorted(lista1) 
+#--------mediana-------#
 
-posicion = round((len(lista1)/2) -1)
+lista1.sort()
+mediana_index = (cantidad//2)
 
-mediana = lista_ordenada[posicion]
+if (cantidad/2) == 0.5:
+    mediana = lista1[mediana_index]
+else:
+    mediana1 = lista1[mediana_index -1]
+    mediana2 = lista1[mediana_index]
+    mediana = (mediana1+mediana2)/2
 
-print("la lista ordenada es ",lista_ordenada," y la media es de ",media,", con mediana de ",mediana)
+print('lista ordendada',lista1,' y la media es de ',media, " con valor de mediana como ", mediana)
