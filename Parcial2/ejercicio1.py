1d0b3068b34a2f1c94eb6875b6f591278b6ac74a
Alier Fres <Tkwmn@users.noreply.github.com>
2023-05-31 17:48:02 -0400

    31-05

--- Parcial2/ejercicio1.py
+++ Parcial2/ejercicio1.py
@@ -1,6 +1,5 @@
 palab = str(input("Ingrese una palabra: "))
 
-
 if palab[0] == palab[-1] and palab[1] == palab[-2] and palab[2] == palab[-3]:
     print('la palabra ingresasda ES un palindromo')
 else:
